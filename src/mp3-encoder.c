3624fe0a246b4b4729957e517297f9e12ff47796
Nurettin Selim <nurettinselim03@gmail.com>
2026-07-26 20:52:22 +0300

    Add GPL-2.0-or-later license and per-file SPDX headers

--- src/mp3-encoder.c
+++ src/mp3-encoder.c
@@ -1,3 +1,23 @@
+/*
+ * obs-shoutcast - audio-only MP3 streaming output for OBS Studio
+ * Copyright (C) 2026 Nurettin Selim
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "mp3-encoder.h"
 
 #include <libavcodec/avcodec.h>
